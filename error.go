d3dee25be03119be0feb1a4e4d609ef13cd182c4
Charlie Revett <charlierevett@gmail.com>
2016-08-01 17:54:58 +0100

    Migrate Existing API (#1)

---
+++ error.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"regexp"
+)
+
+type (
+	// Error pending comment
+	Error struct {
+		Error string
+	}
+)
+
+// NewError comment pending
+func NewError(err error) *Error {
+	return &Error{err.Error()}
+}
+
+// IsRateLimited comment pending
+func (e *Error) IsRateLimited() bool {
+	rateLimit := regexp.MustCompile(`\(#17\)$`)
+	return rateLimit.MatchString(e.Error)
+}
