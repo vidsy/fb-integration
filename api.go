97e53b4bc96e7e74b2233dcad0f35b5b20414038
Charlie Revett <charlierevett@gmail.com>
2016-08-01 17:50:21 +0100

    Migrate

---
+++ api.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	facebookLib "github.com/huandu/facebook"
+)
+
+const facebookAPIVersion = "v2.7"
+
+type (
+	// API comment pending
+	API struct {
+		Session     *facebookLib.Session
+		AccessToken string
+	}
+)
+
+// NewAPI comment pending
+func NewAPI() API {
+	api := API{&facebookLib.Session{}, ""}
+	api.Session.Version = FACEBOOK_API_VERSION
+	return api
+}
+
+// Batch comment pending
+func (f *API) Batch(params ...facebookLib.Params) ([]*APIResponse, *Error) {
+	var apiResponses []*APIResponse
+
+	results, err := f.Session.BatchApi(params...)
+
+	if err != nil {
+		return []*APIResponse{}, NewError(err)
+	}
+
+	for _, result := range results {
+		batch, err := result.Batch()
+
+		if err != nil {
+			return []*APIResponse{}, NewError(err)
+		}
+
+		apiResponse := NewAPIResponse(f, &batch.Result)
+		apiResponses = append(apiResponses, apiResponse)
+	}
+
+	return apiResponses, nil
+}
+
+//Get comment pending
+func (f *API) Get(path string, params facebookLib.Params) (*APIResponse, *Error) {
+	results, err := f.Session.Get(path, params)
+
+	if err != nil {
+		wrappedError := NewError(err)
+		return nil, wrappedError
+	}
+
+	return NewAPIResponse(f, &results), nil
+}
+
+// SetAccessToken comment pending
+func (f *API) SetAccessToken(accessToken string) {
+	f.Session.SetAccessToken(accessToken)
+	f.AccessToken = accessToken
+}
---
+++ api_response.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	facebookLib "github.com/huandu/facebook"
+)
+
+type (
+	// APIResponse comment pending
+	APIResponse struct {
+		*API
+		Result      *facebookLib.Result
+		PagedResult *facebookLib.PagingResult
+		Page        int
+	}
+)
+
+// NewAPIResponse comment pending
+func NewAPIResponse(facebookAPI *API, results *facebookLib.Result) *APIResponse {
+	return &APIResponse{
+		facebookAPI,
+		results,
+		nil,
+		1,
+	}
+}
+
+// Data comment pending
+func (a *APIResponse) Data(paginate bool) ([]facebookLib.Result, error) {
+	if paginate {
+		if a.PagedResult == nil {
+			paging, err := a.Result.Paging(a.Session)
+			if err != nil {
+				return []facebookLib.Result{}, err
+			}
+
+			a.PagedResult = paging
+		} else {
+			a.PagedResult.Next()
+		}
+
+		a.Page++
+		return a.PagedResult.Data(), nil
+	}
+
+	var results []facebookLib.Result
+	results = append(results, *a.Result)
+
+	return results, nil
+}
+
+// HasResults comment pending
+func (a *APIResponse) HasResults() bool {
+	if a.PagedResult == nil {
+		return true
+	}
+	return a.PagedResult.HasNext()
+}
