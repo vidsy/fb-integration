e1e35574f1cdb893153f9a6843b9d8749d443835
Charlie Revett <charlierevett@gmail.com>
2016-08-01 17:42:45 +0100

    🔥 - fb-integration.models.Post{}

---
+++ models/post.go
@@ -0,0 +1,9 @@
+package models
+
+type (
+	// Post comment pending
+	Post struct {
+		ID       string `facebook:"id"        json:"post_id"`
+		ObjectID string `facebook:"object_id" json:"object_id"`
+	}
+)
