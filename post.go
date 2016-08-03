effd4ce71ab59b580586cdf766b16b057997ff50
Charlie Revett <charlierevett@gmail.com>
2016-08-03 18:52:52 +0100

    Add lots of new {} attributes to Post{}

--- post.go
+++ post.go
@@ -9,11 +9,26 @@ import (
 type (
 	// Post comment pending
 	Post struct {
-		ID                string `facebook:"id"        json:"post_id"`
-		ObjectID          string `facebook:"object_id" json:"object_id"`
-		Insights          *facebookLib.Result
-		ReactionBreakdown []*facebookLib.Result
-		TotalReactions    *facebookLib.Result
+		ID                 string `facebook:"id"        json:"post_id"`
+		ObjectID           string `facebook:"object_id" json:"object_id"`
+		Insights           *facebookLib.Result
+		ReactionBreakdown  []*facebookLib.Result
+		TotalReactions     *facebookLib.Result
+		Name               string         `json:"name"`
+		Impressions        int            `json:"impressions"`
+		PaidImpressions    int            `json:"paid_impressions"`
+		OrganicImpressions int            `json:"organic_impressions"`
+		Reach              int            `json:"reach"`
+		PaidReach          int            `json:"paid_reach"`
+		OrganicReach       int            `json:"organic_reach"`
+		VideoViews         int            `json:"video_views"`
+		PaidVideoViews     int            `json:"paid_video_views"`
+		OrganicVideoViews  int            `json:"organic_video_views"`
+		UniqueVideoViews   int            `json:"unique_video_views"`
+		MinutesViewed      int            `json:"minutes_viewed"`
+		AverageDuration    int            `json:"average_duration"`
+		ReactionsTotal     int            `json:"reactions_total"`
+		Reactions          map[string]int `json:"reactions"`
 	}
 )
 
