cdb22b6b1c8c0eb6a030bd4221d75b387821ad70
Charlie Revett <charlierevett@gmail.com>
2016-08-03 18:07:52 +0100

    Total and Breakdown params for Reactions

--- post.go
+++ post.go
@@ -9,9 +9,11 @@ import (
 type (
 	// Post comment pending
 	Post struct {
-		ID       string `facebook:"id"        json:"post_id"`
-		ObjectID string `facebook:"object_id" json:"object_id"`
-		Insights *facebookLib.Result
+		ID                string `facebook:"id"        json:"post_id"`
+		ObjectID          string `facebook:"object_id" json:"object_id"`
+		Insights          *facebookLib.Result
+		ReactionBreakdown []*facebookLib.Result
+		TotalReactions    *facebookLib.Result
 	}
 )
 
@@ -20,5 +22,33 @@ func (p Post) GenerateInsightParams() facebookLib.Params {
 	return facebookLib.Params{
 		"method":       facebookLib.GET,
 		"relative_url": fmt.Sprintf("%s/insights/post_impressions,post_impressions_paid,post_impressions_unique,post_impressions_paid_unique,post_video_views_paid,post_video_views_organic,post_video_views_organic_unique,post_video_views_paid_unique,post_video_view_time,post_video_avg_time_watched?period=lifetime&limit=20", p.ID),
+	}
+}
+
+// GenerateReactionBreakdownParams comment pending
+func (p Post) GenerateReactionBreakdownParams() []facebookLib.Params {
+	var params []facebookLib.Params
+
+	reactions := []string{
+		"LIKE", "LOVE", "WOW", "HAHA", "SAD", "ANGRY", "THANKFUL",
+	}
+
+	for _, reaction := range reactions {
+		params = append(params,
+			facebookLib.Params{
+				"method":       facebookLib.GET,
+				"relative_url": fmt.Sprintf("%s/reactions?limit=0&summary=total_count&type=%s", p.ID, reaction),
+			},
+		)
+	}
+
+	return params
+}
+
+// GenerateTotalReactionsParams comment pending
+func (p Post) GenerateTotalReactionsParams() facebookLib.Params {
+	return facebookLib.Params{
+		"method":       facebookLib.GET,
+		"relative_url": fmt.Sprintf("%s/reactions?limit=0&summary=total_count", p.ID),
 	}
 }
