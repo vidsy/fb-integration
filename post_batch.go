2617613877e56413d4b965d26260a80eb2a97c39
Charlie Revett <charlierevett@gmail.com>
2016-08-03 14:25:23 +0100

    PostBatch for Insights call (#4)

---
+++ post_batch.go
@@ -0,0 +1,48 @@
+package fbintegration
+
+import facebookLib "github.com/huandu/facebook"
+
+type (
+	// PostBatch comment pending
+	PostBatch struct {
+		Posts []Post
+	}
+)
+
+//GeneratePostBatchSlices comment pending
+func GeneratePostBatchSlices(posts []Post, size int) []PostBatch {
+	var postBatches []PostBatch
+
+	amount := (len(posts) / size) + 1
+	start := 0
+
+	var end int
+	if len(posts) < size {
+		end = len(posts)
+	} else {
+		end = size
+	}
+
+	for i := 0; i < amount; i++ {
+		batch := PostBatch{
+			posts[start:end],
+		}
+
+		postBatches = append(postBatches, batch)
+		start += size
+		end += size
+	}
+
+	return postBatches
+}
+
+// InsightParams comment pending
+func (p PostBatch) InsightParams() []facebookLib.Params {
+	var params []facebookLib.Params
+
+	for i := 0; i < len(p.Posts); i++ {
+		params = append(params, p.Posts[i].GenerateInsightParams())
+	}
+
+	return params
+}
