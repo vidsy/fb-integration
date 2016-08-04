15533907c1980b7191f00f7add95e83f5939b8a0
Charlie Revett <charlierevett@gmail.com>
2016-08-04 17:14:32 +0100

    Fragment structure of Post{}

--- post.go
+++ post.go
@@ -10,26 +10,30 @@ import (
 type (
 	// Post comment pending
 	Post struct {
-		ID                 string `facebook:"id"        json:"post_id"`
-		ObjectID           string `facebook:"object_id" json:"object_id"`
-		Insights           *facebookLib.Result
-		ReactionBreakdown  []*facebookLib.Result
-		TotalReactions     *facebookLib.Result
-		Name               string         `json:"name"`
-		Impressions        int            `json:"impressions"`
-		PaidImpressions    int            `json:"paid_impressions"`
-		OrganicImpressions int            `json:"organic_impressions"`
-		Reach              int            `json:"reach"`
-		PaidReach          int            `json:"paid_reach"`
-		OrganicReach       int            `json:"organic_reach"`
-		VideoViews         int            `json:"video_views"`
-		PaidVideoViews     int            `json:"paid_video_views"`
-		OrganicVideoViews  int            `json:"organic_video_views"`
-		UniqueVideoViews   int            `json:"unique_video_views"`
-		MinutesViewed      int            `json:"minutes_viewed"`
-		AverageDuration    int            `json:"average_duration"`
-		ReactionsTotal     int            `json:"reactions_total"`
-		Reactions          map[string]int `json:"reactions"`
+		ID       string `facebook:"id"        json:"post_id"`
+		ObjectID string `facebook:"object_id" json:"object_id"`
+		Name     string `json:"name"`
+		Results  struct {
+			Insights          *facebookLib.Result
+			ReactionBreakdown []*facebookLib.Result
+			TotalReactions    *facebookLib.Result
+		}
+		Data struct {
+			Impressions        int            `json:"impressions"`
+			PaidImpressions    int            `json:"paid_impressions"`
+			OrganicImpressions int            `json:"organic_impressions"`
+			Reach              int            `json:"reach"`
+			PaidReach          int            `json:"paid_reach"`
+			OrganicReach       int            `json:"organic_reach"`
+			VideoViews         int            `json:"video_views"`
+			PaidVideoViews     int            `json:"paid_video_views"`
+			OrganicVideoViews  int            `json:"organic_video_views"`
+			UniqueVideoViews   int            `json:"unique_video_views"`
+			MinutesViewed      int            `json:"minutes_viewed"`
+			AverageDuration    int            `json:"average_duration"`
+			ReactionsTotal     int            `json:"reactions_total"`
+			Reactions          map[string]int `json:"reactions"`
+		} `json:"data"`
 	}
 )
 
@@ -69,78 +73,78 @@ func (p Post) GenerateTotalReactionsParams() facebookLib.Params {
 func (p *Post) ParseResults() {
 	impressions := p.getInsightsValue("post_impressions")
 	if impressions != nil {
-		p.Impressions = int(impressions["value"].(float64))
+		p.Data.Impressions = int(impressions["value"].(float64))
 	}
 
 	paidImpressions := p.getInsightsValue("post_impressions_paid")
 	if impressions != nil {
-		p.PaidImpressions = int(paidImpressions["value"].(float64))
+		p.Data.PaidImpressions = int(paidImpressions["value"].(float64))
 	}
 
-	p.OrganicImpressions = (p.Impressions - p.PaidImpressions)
+	p.Data.OrganicImpressions = (p.Data.Impressions - p.Data.PaidImpressions)
 
 	reach := p.getInsightsValue("post_impressions_unique")
 	if impressions != nil {
-		p.Reach = int(reach["value"].(float64))
+		p.Data.Reach = int(reach["value"].(float64))
 	}
 
 	paidReach := p.getInsightsValue("post_impressions_paid_unique")
 	if impressions != nil {
-		p.PaidReach = int(paidReach["value"].(float64))
+		p.Data.PaidReach = int(paidReach["value"].(float64))
 	}
 
-	p.OrganicReach = (p.Reach - p.PaidReach)
+	p.Data.OrganicReach = (p.Data.Reach - p.Data.PaidReach)
 
 	paidViews := p.getInsightsValue("post_video_views_paid")
 	if paidViews != nil {
-		p.PaidVideoViews = int(paidViews["value"].(float64))
+		p.Data.PaidVideoViews = int(paidViews["value"].(float64))
 	}
 
 	organicViews := p.getInsightsValue("post_video_views_organic")
 	if organicViews != nil {
-		p.OrganicVideoViews = int(organicViews["value"].(float64))
+		p.Data.OrganicVideoViews = int(organicViews["value"].(float64))
 	}
 
-	p.VideoViews = (p.PaidVideoViews + p.OrganicVideoViews)
+	p.Data.VideoViews = (p.Data.PaidVideoViews + p.Data.OrganicVideoViews)
 
 	uniquePaidViews := int(p.getInsightsValue("post_video_views_paid_unique")["value"].(float64))
 	uniqueOrganicViews := int(p.getInsightsValue("post_video_views_organic_unique")["value"].(float64))
 
-	p.UniqueVideoViews = (uniquePaidViews + uniqueOrganicViews)
+	p.Data.UniqueVideoViews = (uniquePaidViews + uniqueOrganicViews)
 
 	minutesViewed := p.getInsightsValue("post_video_view_time")
 	if organicViews != nil {
-		p.MinutesViewed = int(minutesViewed["value"].(float64))
+		p.Data.MinutesViewed = int(minutesViewed["value"].(float64))
 	}
 
 	averageDuration := p.getInsightsValue("post_video_avg_time_watched")
 	if organicViews != nil {
-		p.AverageDuration = int(averageDuration["value"].(float64))
+		p.Data.AverageDuration = int(averageDuration["value"].(float64))
 	}
 
-	p.ReactionsTotal = p.getReactionsTotal(p.TotalReactions)
+	p.Data.ReactionsTotal = p.getReactionsTotal(p.Results.TotalReactions)
 
-	p.Reactions = make(map[string]int)
+	p.Data.Reactions = make(map[string]int)
 	for i, reactionType := range p.reactionTypes() {
-		p.Reactions[reactionType] = p.getReactionsTotal(p.ReactionBreakdown[i])
+		p.Data.Reactions[reactionType] = p.getReactionsTotal(p.Results.ReactionBreakdown[i])
 	}
 }
 
 func (p Post) getInsightsValue(key string) map[string]interface{} {
-	data := p.Insights.Get("data")
+	data := p.Results.Insights.Get("data")
 	slice := reflect.ValueOf(data)
 
 	for i := 0; i < slice.Len(); i++ {
 		query := fmt.Sprintf("data.%d.name", i)
-		name := p.Insights.Get(query)
+		name := p.Results.Insights.Get(query)
 
 		if name == key {
 			query = fmt.Sprintf("data.%d.values", i)
-			values := p.Insights.Get(query).([]interface{})
+			values := p.Results.Insights.Get(query).([]interface{})
 
 			if len(values) > 0 {
 				query = fmt.Sprintf("data.%d.values.0", i)
-				return p.Insights.Get(query).(map[string]interface{})
+				return p.Results.Insights.Get(query).(map[string]interface{})
 			}
 
 			return nil
