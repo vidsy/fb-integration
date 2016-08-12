6ce14f2931eeb57b3924afd676e21962865cc883
Steven Jack <stevenmajack@gmail.com>
2016-08-12 13:22:30 +0100

    Adds top view based on engagement stat

--- posts_summary.go
+++ posts_summary.go
@@ -5,25 +5,26 @@ import "encoding/json"
 type (
 	// PostsSummary comment pending
 	PostsSummary struct {
-		TotalImpressions                   int            `json:"total_impressions"`
-		TotalPaidImpressions               int            `json:"total_paid_impressions"`
-		TotalOrganicImpressions            int            `json:"total_organic_impressions"`
-		TotalReach                         int            `json:"total_reach"`
-		TotalPaidReach                     int            `json:"total_paid_reach"`
-		TotalOrganicReach                  int            `json:"total_organic_reach"`
-		TotalVideoViews                    int            `json:"total_video_views"`
-		TotalPaidVideoViews                int            `json:"total_paid_video_views"`
-		TotalOrganicVideoViews             int            `json:"total_organic_video_views"`
-		TotalUniqueVideoViews              int            `json:"total_unique_video_views"`
-		TotalMinutesViewed                 int            `json:"total_minutes_viewed"`
-		TotalVideosUsed                    int            `json:"total_videos_used"`
-		ReactionsTotal                     int            `json:"reactions_total"`
-		Reactions                          map[string]int `json:"reactions"`
-		TotalClicks                        int            `json:"total_clicks"`
-		TotalUniquePeopleEngaged           int            `json:"total_unique_people_engaged"`
-		TotalActions                       int64          `json:"total_actions"`
-		TotalEngagementPercentPeopleViewed float64        `json:"total_engagement_percent_people_viewed"`
-		TotalViewRate                      float64        `json:"total_view_rate"`
+		TotalImpressions                        int            `json:"total_impressions"`
+		TotalPaidImpressions                    int            `json:"total_paid_impressions"`
+		TotalOrganicImpressions                 int            `json:"total_organic_impressions"`
+		TotalReach                              int            `json:"total_reach"`
+		TotalPaidReach                          int            `json:"total_paid_reach"`
+		TotalOrganicReach                       int            `json:"total_organic_reach"`
+		TotalVideoViews                         int            `json:"total_video_views"`
+		TotalPaidVideoViews                     int            `json:"total_paid_video_views"`
+		TotalOrganicVideoViews                  int            `json:"total_organic_video_views"`
+		TotalUniqueVideoViews                   int            `json:"total_unique_video_views"`
+		TotalMinutesViewed                      int            `json:"total_minutes_viewed"`
+		TotalVideosUsed                         int            `json:"total_videos_used"`
+		ReactionsTotal                          int            `json:"reactions_total"`
+		Reactions                               map[string]int `json:"reactions"`
+		TotalClicks                             int            `json:"total_clicks"`
+		TotalUniquePeopleEngaged                int            `json:"total_unique_people_engaged"`
+		TotalActions                            int64          `json:"total_actions"`
+		TotalEngagementPercentPeopleViewed      float64        `json:"total_engagement_percent_people_viewed"`
+		TotalViewRate                           float64        `json:"total_view_rate"`
+		TopEngagementPercentPeopleViewedVideoID string         `json:"top_engagement_percent_people_viewed_video_id"`
 	}
 )
 
@@ -56,6 +57,7 @@ func NewPostsSummary(posts []*Post) PostsSummary {
 	}
 
 	ps.TotalVideosUsed = calculateVideosUsed(posts)
+	ps.TopEngagementPercentPeopleViewedVideoID = findTopVideoFromEngagementPercentPeopleViewedVideo(posts)
 
 	return ps
 }
@@ -80,4 +82,17 @@ func calculateVideosUsed(posts []*Post) int {
 	}
 
 	return len(videosUsed)
+}
+
+func findTopVideoFromEngagementPercentPeopleViewedVideo(posts []*Post) string {
+	topPost := posts[0]
+
+	for _, post := range posts {
+		if post.Data.EngagementPercentPeopleViewed > topPost.Data.EngagementPercentPeopleViewed {
+			topPost = post
+		}
+	}
+
+	return topPost.ObjectID
+
 }
