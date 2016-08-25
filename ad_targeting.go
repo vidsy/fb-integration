00149cc575459b92b9be525a350d51e25ce16978
Steven Jack <stevenmajack@gmail.com>
2016-08-25 13:52:38 +0100

    Remove outter if

--- ad_targeting.go
+++ ad_targeting.go
@@ -19,44 +19,41 @@ type (
 
 // NewAdTargetingFromResult comment pending
 func NewAdTargetingFromResult(results *facebookLib.Result) AdTargeting {
-	targeting := results.Get("targeting")
 	query := gountries.New()
 	adTargeting := AdTargeting{}
 
-	if targeting != nil {
-		countries := results.Get("targeting.geo_locations.countries")
-		if countries != nil {
-			countryList := reflect.ValueOf(countries)
-			for i := 0; i < countryList.Len(); i++ {
-				country := results.Get(fmt.Sprintf("targeting.geo_locations.countries.%d", i)).(string)
-				countryName, err := query.FindCountryByAlpha(country)
-				if err != nil {
-					adTargeting.Locations = append(adTargeting.Locations, country)
-				} else {
-					adTargeting.Locations = append(adTargeting.Locations, countryName.Name.Common)
-				}
+	countries := results.Get("targeting.geo_locations.countries")
+	if countries != nil {
+		countryList := reflect.ValueOf(countries)
+		for i := 0; i < countryList.Len(); i++ {
+			country := results.Get(fmt.Sprintf("targeting.geo_locations.countries.%d", i)).(string)
+			countryName, err := query.FindCountryByAlpha(country)
+			if err != nil {
+				adTargeting.Locations = append(adTargeting.Locations, country)
+			} else {
+				adTargeting.Locations = append(adTargeting.Locations, countryName.Name.Common)
 			}
 		}
+	}
 
-		ageMin := results.Get("targeting.age_min")
-		if ageMin != nil {
-			adTargeting.AgeMin = ageMin.(float64)
-		}
+	ageMin := results.Get("targeting.age_min")
+	if ageMin != nil {
+		adTargeting.AgeMin = ageMin.(float64)
+	}
 
-		ageMax := results.Get("targeting.age_max")
-		if ageMax != nil {
-			adTargeting.AgeMax = ageMax.(float64)
-		}
+	ageMax := results.Get("targeting.age_max")
+	if ageMax != nil {
+		adTargeting.AgeMax = ageMax.(float64)
+	}
 
-		interests := results.Get("targeting.interests")
-		if interests != nil {
-			adTargeting.Interests = addInterests(interests, results)
-		}
+	interests := results.Get("targeting.interests")
+	if interests != nil {
+		adTargeting.Interests = addInterests(interests, results)
+	}
 
-		flexibleInterests := results.Get("targeting.flexible_spec.interests")
-		if flexibleInterests != nil {
-			adTargeting.Interests = addInterests(flexibleInterests, results)
-		}
+	flexibleInterests := results.Get("targeting.flexible_spec.interests")
+	if flexibleInterests != nil {
+		adTargeting.Interests = addInterests(flexibleInterests, results)
 	}
 
 	return adTargeting
